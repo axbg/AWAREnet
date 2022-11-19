8e5c50b78db9b7cc46dc33d329cc1f5c908b8740
Stefan <lazeastefan@gmail.com>
2022-11-19 16:41:39 +0200

    Add colors

--- front/src/App.js
+++ front/src/App.js
@@ -1,50 +1,50 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Home } from './Home';
 import { ResponsiveAppBar } from './components/navigation-bar/ResponsiveAppBar';
 import { GlobalContextProvide } from './global-context';
 import './App.scss';
 
 function App() {
-    // const darkTheme = createTheme({
-    //     typography: {
-    //         fontFamily: 'Roboto'
-    //     },
-    //     palette: {
-    //         primary: {
-    //             main: '#4281A4'
-    //         },
-    //         secondary: {
-    //             main: '#E6B89C'
-    //         },
-    //         tertiary: {
-    //             main: '#FFFFFF'
-    //         },
-    //         error: {
-    //             main: '#da3e52'
-    //         },
-    //         warning: {
-    //             main: '#F2E94E'
-    //         },
-    //         info: {
-    //             main: '#96e6b3' //'#42D9C8'
-    //         }
-    //     }
-    // });
+    const darkTheme = createTheme({
+        // typography: {
+        //     fontFamily: 'Roboto'
+        // },
+        palette: {
+            primary: {
+                main: '#0A3200'
+            },
+            secondary: {
+                main: '#114b5f'
+            },
+            tertiary: {
+                main: '#FFFFFF'
+            },
+            error: {
+                main: '#da3e52'
+            },
+            warning: {
+                main: '#F2E94E'
+            },
+            info: {
+                main: '#96e6b3' //'#42D9C8'
+            }
+        }
+    });
 
     return (
         <GlobalContextProvide>
             <ResponsiveAppBar />
-            {/* <ThemeProvider theme={darkTheme}> */}
-            <div className="app">
-                {/* DO NOT USE component like bellow in a Switch statement */}
-                <Routes>
-                    <Route path="/" exact element={<Home />} />
-                </Routes>
-            </div>
-            {/* </ThemeProvider> */}
+            <ThemeProvider theme={darkTheme}>
+                <div className="app">
+                    {/* DO NOT USE component like bellow in a Switch statement */}
+                    <Routes>
+                        <Route path="/" exact element={<Home />} />
+                    </Routes>
+                </div>
+            </ThemeProvider>
         </GlobalContextProvide>
     );
 }
--- front/src/Home.js
+++ front/src/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 export const Home = () => {
-    return <div>Hello</div>;
+    return (
+        <div className="home">
+            <Button variant="contained">Contained</Button>
+        </div>
+    );
 };
