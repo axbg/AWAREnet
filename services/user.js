178ecd2b4f59841f3633c39e5472066467c52e0e
axbg <bisagalexstefan@gmail.com>
2022-11-19 16:31:09 +0200

    Removed sequelize usages

--- services/user.js
+++ services/user.js
@@ -4,9 +4,6 @@ const checkAuthentication = async (email, password) => {
   // Mongoose
   const user = await UserModel.findOne({email: email});
 
-  // Sequelize
-  // const user = await UserModel.findOne({where: {email: email}});
-
   return user ? ((await user.comparePassword(password)) ? user.id : false) :
       (await UserModel.create({email: email, password: password})).id;
 };
@@ -14,9 +11,6 @@ const checkAuthentication = async (email, password) => {
 const getOrCreateUser = async (profile) => {
   // Mongoose
   const existingUser = await UserModel.findOne({email: profile.email});
-
-  // Sequelize
-  // const existingUser = await UserModel.findOne({where: {email: profile.email}});
 
   if (existingUser) {
     return {_id: existingUser._id};
