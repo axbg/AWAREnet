8713944d379addc9dc7229969f833750ac6e3d8c
Stefan <lazeastefan@gmail.com>
2022-11-20 03:17:23 +0200

    Add login

--- front/src/pages/auth/Login.jsx
+++ front/src/pages/auth/Login.jsx
@@ -1,12 +1,54 @@
 import React, { useEffect } from 'react';
 import { addUser, useGlobalContext } from '../../global-context';
+import { Box, Grid, Button, TextField } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import illustration from 'assets/join-login.svg';
 
-export const Login = () => {
+const Login = () => {
+    const navigate = useNavigate();
     const { dispatch } = useGlobalContext();
 
     useEffect(() => {
         console.log('here');
         dispatch(addUser({ userId: '2345678', role: 'NGO' }));
     }, []);
-    return <div>HELO</div>;
+
+    const goToRegister = () => {
+        navigate('/register', { replace: false });
+    };
+
+    const goToDashboard = () => {
+        //handle login
+        navigate('/', { replace: false });
+    };
+
+    return (
+        <div className="login-container">
+            <img src={illustration} alt="avatar" className="login-img" />
+            <Grid item xs={6} pl={0}>
+                <h1 className="page-title">{'Login'}</h1>
+                <div className="login-inputs">
+                    <TextField label="Email" />
+                    <TextField label="Password" type="password" />
+                </div>
+                <div className="login-buttons">
+                    <Button
+                        sx={{ textTransform: 'none', marginRight: '0px' }}
+                        variant="outlined"
+                        onClick={goToDashboard}>
+                        {"Let's go"}
+                    </Button>
+                    <Button
+                        sx={{ textTransform: 'none' }}
+                        variant="outlined"
+                        color="secondary"
+                        onClick={goToRegister}>
+                        Create an account
+                    </Button>
+                </div>
+            </Grid>
+        </div>
+    );
 };
+
+export { Login };
