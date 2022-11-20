c6c1a0424c90c2b89f86f4165e3e6a74b7552ca7
Stefan <lazeastefan@gmail.com>
2022-11-20 02:04:42 +0200

    Add image gallery

--- front/src/pages/action/components/image-list/ImageListModal.jsx
+++ front/src/pages/action/components/image-list/ImageListModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImageGallery from 'react-image-gallery';
 
 import {
     Dialog,
@@ -6,92 +7,27 @@ import {
     DialogContent,
     DialogContentText,
     DialogTitle,
-    Button,
-    ImageList,
-    ImageListItem,
-    ImageListItemBar,
-    IconButton
+    Button
 } from '@mui/material';
-
-import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 import styles from './ImageListModal.module.scss';
 import _ from 'lodash';
 
 export const ImageListModal = (props) => {
     const { isOpen, handleClose, action } = props;
-    function srcset(image, width, height, rows = 1, cols = 1) {
-        return {
-            src: `${image}?w=${width * cols}&h=${
-                height * rows
-            }&fit=crop&auto=format`,
-            srcSet: `${image}?w=${width * cols}&h=${
-                height * rows
-            }&fit=crop&auto=format&dpr=2 2x`
-        };
-    }
-    const itemData = [
+
+    const images = [
         {
-            img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-            title: 'Breakfast',
-            author: '@bkristastucchio',
-            featured: true
+            original: 'https://picsum.photos/id/1018/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1018/250/150/'
         },
         {
-            img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-            title: 'Burger',
-            author: '@rollelflex_graphy726'
+            original: 'https://picsum.photos/id/1015/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1015/250/150/'
         },
         {
-            img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-            title: 'Camera',
-            author: '@helloimnik'
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-            title: 'Coffee',
-            author: '@nolanissac'
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-            title: 'Hats',
-            author: '@hjrc33'
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-            title: 'Honey',
-            author: '@arwinneil',
-            featured: true
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-            title: 'Basketball',
-            author: '@tjdragotta'
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-            title: 'Fern',
-            author: '@katie_wasserman'
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-            title: 'Mushrooms',
-            author: '@silverdalex'
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-            title: 'Tomato basil',
-            author: '@shelleypauls'
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-            title: 'Sea star',
-            author: '@peterlaster'
-        },
-        {
-            img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-            title: 'Bike',
-            author: '@southside_customs'
+            original: 'https://picsum.photos/id/1019/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1019/250/150/'
         }
     ];
 
@@ -106,50 +42,9 @@ export const ImageListModal = (props) => {
                 <DialogContentText>
                     {_.get(action, 'description', "See what's has improved")}
                 </DialogContentText>
-                <ImageList
-                    sx={{
-                        width: 500,
-                        height: 450,
-                        // Promote the list into its own layer in Chrome. This costs memory, but helps keeping high FPS.
-                        transform: 'translateZ(0)'
-                    }}
-                    rowHeight={200}
-                    gap={1}>
-                    {itemData.map((item) => {
-                        const cols = item.featured ? 2 : 1;
-                        const rows = item.featured ? 2 : 1;
-
-                        return (
-                            <ImageListItem
-                                key={item.img}
-                                cols={cols}
-                                rows={rows}>
-                                <img
-                                    {...srcset(item.img, 250, 200, rows, cols)}
-                                    alt={item.title}
-                                    loading="lazy"
-                                />
-                                <ImageListItemBar
-                                    sx={{
-                                        background:
-                                            'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-                                            'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)'
-                                    }}
-                                    title={item.title}
-                                    position="top"
-                                    actionIcon={
-                                        <IconButton
-                                            sx={{ color: 'white' }}
-                                            aria-label={`star ${item.title}`}>
-                                            <StarBorderIcon />
-                                        </IconButton>
-                                    }
-                                    actionPosition="left"
-                                />
-                            </ImageListItem>
-                        );
-                    })}
-                </ImageList>
+                <div className={styles.imageContainer}>
+                    <ImageGallery items={images} />
+                </div>
             </DialogContent>
             <DialogActions>
                 <Button onClick={() => handleClose()}>Close</Button>
